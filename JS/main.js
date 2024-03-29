c4ca7b48820c544441b37c520db14402f674a773
Cody Miller <C.JMiller17@yahoo.com>
2024-03-28 22:36:49 -0400

    Added the digital clock and several functions

--- JS/main.js
+++ JS/main.js
@@ -1,16 +1,52 @@
-const time = new Date();
-const second = 0;
-const minute = 0;
-const hour = 0;
+/* This function runs the analog clock. */
 
 setInterval(
     function() {
         date = new Date();
         sec = date.getSeconds() * 6;
         min = date.getMinutes() * 6;
-        hor = date.getHours() * 30 + Math.round(minute /12);
+        hor = date.getHours() * 30 + Math.round(min /12);
         document.getElementById("second-hand").style.transform = "rotate(" + sec + "deg)"
         document.getElementById("minute-hand").style.transform = "rotate(" + min + "deg)"
         document.getElementById("hour-hand").style.transform = "rotate(" + hor + "deg)";
-    }, 1000
+    }
 )
+
+/* This will power the digital clock */
+
+setInterval(function () {
+    const time = new Date();
+    const second = time.getSeconds();
+    const minute = time.getMinutes();
+    const hour = time.getHours();
+    document.querySelector(".display").textContent = hour + ":" + minute + ":" + second;
+}, 1000);
+
+/* This will push times into the alarm array and display them */
+
+let setAlarms = [];
+
+function addAlarm() {
+    const hh = document.querySelector("select[name='hourInput']").value;
+    const mm = document.querySelector("select[name='minuteInput']").value;
+    const ampm = document.querySelector("select[name='ampm']").value;
+    
+    let alarm = hh + ":" + mm + " " + ampm;
+    setAlarms.push(alarm);
+    displayAlarms();
+}
+
+function displayAlarms() {
+    const alarmsContainer = document.querySelector(".alarms");
+    alarmsContainer.innerHTML = ""; // Clear previous content
+    
+    // Loop through the array and display each alarm
+    setAlarms.forEach(function(alarm, index) {
+        const alarmElement = document.createElement("div");
+        alarmElement.textContent = "Alarm " + (index + 1) + ": " + alarm;
+        alarmsContainer.appendChild(alarmElement);
+    });
+}
+
+// Attach event listener to the "Add Alarm" button
+document.getElementById("set").addEventListener("click", addAlarm);
